84674c4cb0aa834bc052a054b18b237fae6569f5
Matheus Liberato <matheus.liberatoo@gmail.com>
2022-05-23 11:09:28 -0300

    Removendo Bug do app

--- app.py
+++ app.py
@@ -6,14 +6,5 @@ app = Flask(__name__)
 def pagina_inicial():
     return "Custom Menssage"
 
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")  
-
 if __name__ == '__main__':
     app.run()
